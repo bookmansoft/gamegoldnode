11f2bc91184c71b99df8811afe1ba61e23240a3e
bianque <bianque@qq.com>
2019-05-22 22:33:32 +0800

    1.5.26

--- test/online/acl.js
+++ test/online/acl.js
@@ -45,6 +45,20 @@ remoteOperator.setFetch(require('node-fetch'))  //兼容性设置，提供模拟
     structured: true,
 });
 
+//创建用于事件监控的长连模式的连接器，并设置相应的参数
+const monitor = new toolkit.conn();
+monitor.setFetch(require('node-fetch'))  //兼容性设置，提供模拟浏览器环境中的 fetch 函数
+.setup({
+    type:   'testnet',
+    ip:     '127.0.0.1',          //远程服务器地址
+    head:   'http',               //远程服务器通讯协议，分为 http 和 https
+    id:     'primary',            //默认访问的钱包编号
+    apiKey: 'bookmansoft',        //远程服务器基本校验密码
+    cid: env.rootName, 
+    token: env.rootToken,
+    structured: true,
+});
+
 describe.only('操作员管理', () => {
     it('管理员为操作员分配令牌', async () => {
         env.opName = uuid(); //生成随机的操作员账号
@@ -74,5 +88,24 @@ describe.only('操作员管理', () => {
         //操作员得到了ACL授权，操作成功
         let ret = await remoteOperator.execute('address.create', []);
         assert(!ret.error);
+    });
+
+    it('通过监控节点重启事件，重新设定ACL', async () => {
+        //订阅并监控主网重启事件
+        await monitor.setmode(monitor.CommMode.ws).watch(async function(msg) {
+            //当捕获到重启事件时，重新设定ACL
+            console.log('Info: Got event chain/full, Try to reset ACL...');
+
+            let ret = await remote.execute('sys.groupPrefix', [[['address', env.opName]]]);
+            assert(!ret.error);
+
+            console.log('Info: Finished Reset ACL.');
+        }, 'chain/full').execute('subscribe', ['chain/full']);
+
+        //强制设置同步完成标志，触发全节点下发 chain/full 事件
+        await remote.execute('miner.setsync.admin', []);
+
+        //设置一个等待时间，以测试事件捕获机制
+        await (async function(time){return new Promise(resolve =>{setTimeout(resolve, time);});})(1000);
     });
 });
