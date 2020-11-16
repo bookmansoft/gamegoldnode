805771fc7d8fcb1dc970074448b6f6e60fae3aad
bianque@qq.com <bianque@qq.com>
2020-11-16 12:17:22 +0800

    V2.1.0

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const node = new FullNode({
   config: true, // 是否载入外部配置文件
   argv: true,
   env: true,
-  logFile: true,
+  logFile: false,
   logConsole: true,
   logLevel: 'debug',
   db: 'leveldb',
@@ -101,91 +101,6 @@ const node = new FullNode({
     wdb.on('balance.client', msg => {
       //console.log('balance.client', msg);
     });
-
-    //#region 多签合约下发通告处理句柄
-    wdb.on('mssendpubk/receive', async msg => {
-      let wallet = await wdb.get(msg.addr);
-      if(!wallet) { //尚未创建本地多签钱包: 使用下行参数创建
-          wallet = await wdb.ensure({
-              id: msg.addr,         //Wallet ID (used for storage)
-              type: 'multisig',     //Type of wallet (pubkeyhash, multisig) (default=pubkeyhash).
-              m: msg.m,             //`m` value for multisig.
-              n: msg.n,             //`n` value for multisig.
-              witness: true,        //Whether to use witness programs.
-          });
-
-          let [wid, account] = await wdb.getAccountByAddress(msg.addr);
-          let wa = await wdb.get(wid);
-          if(!!wa) {
-            await wa.send({
-                subtractFee: false,
-                sort: false,
-                outputs: [{
-                    address: msg.contract,
-                    value: 10000,
-                }],
-                comment: JSON.stringify({
-                    oper: 'scrun',//合约驱动
-                    params: {
-                        oper: 'pubk',
-                        addr: msg.addr,
-                        puba: wallet.account.accountKey,
-                    },
-                }), 
-            }, account);
-          }
-      }
-
-      if(wallet.account.keys.length < wallet.account.n-1) {
-        //将收到的公钥导入钱包
-        let ks = msg.data.split(',');
-        for(let k of ks) {
-          try {
-            await wallet.addSharedKey(k);
-          } catch(e) {
-
-          }
-        }
-
-        if(wallet.account.keys.length == wallet.account.n-1) { //首次达成公钥集齐，发送[合约驱动:上传地址]
-          let [wid, account] = await wdb.getAccountByAddress(msg.addr);
-          let wa = await wdb.get(wid);
-          if(!!wa) {
-            await wa.send({
-                subtractFee: false,
-                sort: false,
-                outputs: [{
-                    address: msg.contract,
-                    value: 10000,
-                }],
-                comment: JSON.stringify({
-                    oper: 'scrun',//合约驱动
-                    params: {
-                        oper: 'puba',
-                        addr: msg.addr,
-                        puba: wallet.account['receive'].getAddress().toString(),
-                    },
-                }), 
-            }, account);
-          }
-        }
-      }
-    });
-
-    wdb.on('mssendtx/receive', async msg => {
-      //收到[合约衍生：下发签名]，补充签名，如果达到门限要求就广播交易，否则选择发送[合约衍生：上传签名]
-      let wallet = await wdb.get(msg.addr);
-      if(wallet && wallet.account.type == Account.types.MULTISIG) {
-        const mtx = MTX.fromRaw(msg.data, 'hex');
-        mtx.view = await wallet.getCoinView(mtx);
-        if(!mtx.isSigned()) {
-          //暂存待签名交易
-          msg.txid = mtx.txid();
-          await wdb.saveMSTrans(msg);
-        }
-      }
-    });
-    //#endregion
   }
 
   await node.connect();
@@ -194,7 +109,7 @@ const node = new FullNode({
   //通过传入 true/false 开启/关闭挖矿
   node.rpc.execute({method:'miner.set.admin',params:[false]});
 
-  if(node.miner && !node.miner.tokenAddress) {
+  if(node.miner && (!node.miner.addresses || node.miner.addresses.length==0)) {
     await node.rpc.execute({method:'miner.setsync.admin',params:[]});
     await node.rpc.execute({method:'miner.setaddr.admin',params:[]});
   }
@@ -203,7 +118,7 @@ const node = new FullNode({
   startproxy({
     node: node, 
     pow: process.argv.indexOf('--pow') !== -1,
-    ports: [7333, 17333, 17444, 27333, 27901],
+    ports: [2000, 2100],
   });
   //#endregion
 })().catch((err) => {
