fb5e63441f23a33406f63f087566293c202ea1ae
bianque@qq.com <bianque@qq.com>
2020-11-16 12:48:39 +0800

    ...

---
+++ test/意愿存证.js
@@ -0,0 +1,153 @@
+/**
+ * 联机单元测试：意愿存证
+ * Creted by liub 2020.10.30
+ */
+
+const uuid = require('uuid/v1')
+const assert = require('assert');
+const remote = (require('../test/util/connector'))();
+const gamegold = require('gamegold');
+const digest = gamegold.crypto.digest;
+const base58 = gamegold.base58;
+
+let env = {
+    cp: {name: "cp-"+ uuid().slice(0,33), erid:''},
+    alice: {name: "alice-"+ uuid().slice(0,30), erid:'', address:''},
+    content: 'hello world', //存证原始内容
+};
+
+describe('意愿存证', function() {
+    before(async () => {
+        await remote.execute('miner.setsync.admin', [true]);
+        let ret = await remote.execute('block.tips', []);
+        if(ret[0].height < 100) {
+            await remote.execute('miner.generate.admin', [100 - ret[0].height]);
+        }
+        await remote.execute('miner.generate.admin', [1]);
+        await remote.wait(1000);
+    });
+
+    after(() => {
+        remote.close();
+    });
+
+	it('核心节点为企业注册证书', async () => {
+       //注册一个新的CP
+       let ret = await remote.execute('cp.create', [env.cp.name, '127.0.0.1']);
+
+       //确保该CP数据上链
+       await remote.execute('miner.generate.admin', [1]);
+       await remote.wait(1000);
+
+       //查询并打印CP信息
+       ret = await remote.execute('cp.byName', [env.cp.name]);
+	   env.cp.id = ret.cid;
+	   env.cp.pubkey = ret.pubKey;
+	   assert(env.cp.name == ret.name);
+    });
+
+    it('核心节点为企业用户注册证书', async () => {
+		let ret = await remote.execute('cp.user', [env.cp.id, env.alice.name, null, env.cp.id]);
+		env.alice.address = ret.data.addr;
+        env.alice.pubkey = ret.data.pubkey;
+
+        ret = await remote.execute('key.export.private', [env.alice.address, env.cp.id]);
+        assert(!ret.error);
+        env.alice.prikey = base58.decode(ret).slice(1, 33);
+
+		await remote.execute('tx.send', [env.alice.address, 100000000]);
+        await remote.execute('miner.generate.admin', [1]);
+        await remote.wait(1000);
+	});
+
+    it('用户签发意愿存证', async () => {
+		//生成真实意愿存证文件的哈希值
+		let hash = digest.hash256(Buffer.from(env.content)).toString('hex');
+
+		//签发意愿存证
+        let ret = await remote.execute('ca.issue', [
+            env.alice.address,          //签发地址
+            '',                         //name
+            env.cp.pubkey,           	//cp pubkey
+            hash,                		//content hash
+            0,                          //有效期，填0表示使用默认值
+            env.cp.id,
+        ]);
+		assert(ret.erid);
+		//保存存证编号
+        env.cp.erid = ret.erid;
+
+		//确保存证上链
+        await remote.execute('miner.generate.admin', [1]);
+        await remote.wait(1000);
+    });
+
+    it('查询存证：根据存证编号查询存证内容', async () => {
+        let ret = await remote.execute('ca.list', [[['erid', env.cp.erid]]]);
+        assert(ret.list[0].erid == env.cp.erid);
+    });
+
+    it('验证存证：验证存证的有效性 - 成功', async () => {
+        let ret = await remote.execute('ca.verify', [env.cp.erid]);
+        assert(ret && ret.verify);
+    });
+
+    it('废止存证：用户废止先前签发的意愿存证', async () => {
+        let ret = await remote.execute('ca.abolish', [
+            env.alice.address,             //签发地址
+			env.cp.erid,
+			0,
+			env.cp.id,			
+        ]);
+        assert(!ret.error);
+
+        await remote.execute('miner.generate.admin', [1]);
+        await remote.wait(500);
+    });
+
+    it('查询废止：查询存证废止列表', async () => {
+        let ret = await remote.execute('ca.list.ab', [[['erid', env.cp.erid]]]);
+        assert(ret.count == 1);
+    });
+
+    it('验证存证：验证存证的有效性 - 失败', async () => {
+        let ret = await remote.execute('ca.verify', [env.cp.erid]);
+        assert(ret && !ret.verify);
+    });
+
+    it('用户签发意愿存证', async () => {
+        //生成真实意愿存证文件的哈希值
+		let hash = digest.hash256(Buffer.from(env.content)).toString('hex');
+
+		//签发意愿存证
+        let ret = await remote.execute('ca.issue', [
+            env.alice.address,          //签发地址
+            '',                         //name
+            env.cp.pubkey,           	//cp pubkey
+            hash,                		//content hash
+            5,                          //有效期，填0表示使用默认值
+            env.cp.id,
+        ]);
+		assert(ret.erid);
+		//保存存证编号
+        env.cp.erid = ret.erid;
+
+		//确保存证上链
+        await remote.execute('miner.generate.admin', [1]);
+        await remote.wait(1000);
+    });
+
+    it('验证存证：验证存证的有效性 - 成功', async () => {
+        let ret = await remote.execute('ca.verify', [env.cp.erid]);
+        assert(ret && ret.verify);
+    });
+
+    it('存证过期：推进链态高度，使得证书失效', async () => {
+        await remote.execute('miner.generate.admin', [6]);
+    });
+
+    it('验证存证：验证存证的有效性 - 失败', async () => {
+        let ret = await remote.execute('ca.verify', [env.cp.erid]);
+        assert(ret && !ret.verify);
+    });
+});
--- test/节点选举.js
+++ test/节点选举.js
@@ -2,8 +2,10 @@
  * 单元测试: 节点选举
  * @description 
  * 节点可通过如下两种方式获取记账权
- * 1. 获取官方矿产证
+ * 1. 获取官方节点证书
  * 2. 获取社区投票认可
+ * 
+ * 本单元测试展示第2种情形
  */
 
 const assert = require('assert')
@@ -38,35 +40,36 @@ describe('节点选举', () => {
         }
     });
 
-    for(let i = 0; i < 3; i++) {
-        it('验证选举权：投票前，Alice记账失败', async () => {
-            console.log(`第${i+1}轮测试开始`);
-            let ret = await remote.execute('address.create', [env.alice.name]);
-            assert(!ret.error);
-            env.alice.address = ret.address;
+    it('验证选举权：投票前，Alice记账失败', async () => {
+        let ret = await remote.execute('address.create', [env.alice.name]);
+        assert(!ret.error);
+        env.alice.address = ret.address;
 
-            ret = await remote.execute('miner.generateto.admin', [1, env.alice.address]);
-            assert(!!ret.error);
-        });
+        ret = await remote.execute('miner.generateto.admin', [1, env.alice.address]);
+        assert(!!ret.error);
+    });
 
-        it('投票选举：为Alice记账权投票，然后生成足够区块以切换统计区间', async () => {
-            let ret = await remote.execute('vote.send', [env.alice.address, 100000000]);
-            assert(!ret.error);
+    it('投票选举：为Alice记账权投票，然后生成足够区块以切换统计区间', async () => {
+        let ret = await remote.execute('vote.send', [env.alice.address, 100000000]);
+        assert(!ret.error);
 
-            //确保记账权变化生效
-            await remote.execute('miner.generate.admin', [consensus.BLOCK_2WEEK]); // 过大或过小都会导致统计周期意外切换，从而影响投票权的有效性
-        });
+        //确保记账权变化生效
+        await remote.execute('miner.generate.admin', [consensus.retargetInterval]); // 如果不是retargetInterval的整数倍，会导致统计周期意外切换，从而影响投票权的有效性
+    });
 
-        it('查询选举：查询Alice当前选举权', async () => {
-            console.log(env.alice.address);
-            let ret = await remote.execute('vote.check', [env.alice.address]);
-            assert(!ret.error);
-            assert(ret.vote > 0);
-        });
+    it('查询选举：查询Alice当前选举权', async () => {
+        let ret = await remote.execute('vote.check', [env.alice.address]);
+        assert(!ret.error && ret.vote > 0);
+    });
 
-        it('验证选举权：投票后，Alice记账成功', async () => {
-            let ret = await remote.execute('miner.generateto.admin', [1, env.alice.address]);
-            assert(!ret.error);
-        });
-    }
+    it('验证选举权：投票后Alice拥有记账权，并且在竞争不充分的情况下持续有效', async () => {
+        //记账成功，说明Alice拥有记账权
+        let ret = await remote.execute('miner.generateto.admin', [1, env.alice.address]);
+        assert(!ret.error);
+
+        //再经过一个选举周期，Alice记账权仍旧有效(Stick)
+        await remote.execute('miner.generate.admin', [consensus.retargetInterval]);
+        ret = await remote.execute('miner.generateto.admin', [1, env.alice.address]);
+        assert(!ret.error);
+    });
 });
