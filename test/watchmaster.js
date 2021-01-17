166162ec36902991fff3f046d8884725f8642ce8
bianque@qq.com <bianque@qq.com>
2021-01-17 17:06:45 +0800

    ...

---
+++ test/watchmaster.js
@@ -0,0 +1,33 @@
+/**
+ * 联机单元测试：系统监控
+ * Creted by liub 2020.10.30
+ */
+
+const assert = require('assert');
+const remote = (require('../lib/remote/connector'))();
+
+//设定测试所需的环境变量
+let env = {
+};
+
+describe('系统监控', function() {
+    /**
+     * 单元测试模块后置处理流程
+     */
+    after(() => {
+        remote.close();
+    });
+
+    it('系统信息', async () => {
+        while(true) {
+            try {
+                let ret = await remote.execute('block.count', []);
+                console.log('master', ret);
+                console.log('-------------------------');
+
+                await remote.wait(3000);
+            } catch(e) {
+            }
+        }
+     });
+});
